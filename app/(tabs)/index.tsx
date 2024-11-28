db1824effdcdaa7f5151d4bfec81524498cd6fcf
Tristan <tristan.petit01@gmail.com>
2024-11-28 22:52:27 +0100

    ajout du système de filtrage sur le genre et le niveau des utilisateurs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,9 @@ import {
   ActivityIndicator,
   Dimensions,
   Animated,
+  Modal,
+  TouchableOpacity,
+  TextInput,
 } from "react-native";
 import { Text, View } from "@/components/Themed";
 import { useGetLocationsQuery } from "../apis/LocationApi";
@@ -12,6 +15,7 @@ import { useUsers } from "@/hooks/useUsers";
 import MapView, { Marker, Polyline } from "react-native-maps";
 import { User } from "../types/user";
 import { Coordinates } from "../types/coordinates";
+import { ButtonGroup } from 'react-native-elements';
 
 type LocationArray = [number, number];
 
@@ -43,7 +47,6 @@ const findNearestLocation = (
   }, locations[0]);
 };
 
-// Fonction pour calculer la distance entre deux points géographiques
 const calculateDistance = (coord1: Coordinates, coord2: Coordinates) => {
   const R = 6371; // Rayon de la Terre en km
   const dLat = ((coord2.latitude - coord1.latitude) * Math.PI) / 180;
@@ -74,8 +77,13 @@ export default function TabOneScreen() {
   const [fadeAnim] = useState(new Animated.Value(0));
   const [selectedUserCoords, setSelectedUserCoords] = useState<Coordinates | null>(null);
   const [nearestGym, setNearestGym] = useState<any>(null);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [genderFilter, setGenderFilter] = useState<string>("");
+  const [rankFilter, setRankFilter] = useState<string[]>([]);
 
-  // Supprimer les doublons des gymnases
+  const ranks = ['Tous', 'N1', 'N2', 'N3', 'R4', 'R5', 'R6', 'D7', 'D8', 'D9', 'P10', 'P11', 'P12', 'NC'];
+
+
   const filteredGyms = data?.results
     ? data.results.reduce((uniqueGyms, gym) => {
         if (!uniqueGyms.some((g) => g.inst_nom === gym.inst_nom)) {
@@ -90,11 +98,23 @@ export default function TabOneScreen() {
 
     const userCoords = arrayToCoordinates(user.current_location);
 
+    const toggleRank = (rank: string) => {
+      setRankFilter((prev) =>
+        prev.includes(rank) ? prev.filter((r) => r !== rank) : [...prev, rank]
+      );
+    };
+
+    const matchesGender = genderFilter ? user.sexe === genderFilter : true;
+    const matchesRank = rankFilter.length > 0 ? rankFilter.includes(user.ranking) : true;
+
+
     return (
       userCoords.latitude <= cardinalPoints.north.latitude &&
       userCoords.latitude >= cardinalPoints.south.latitude &&
       userCoords.longitude >= cardinalPoints.west.longitude &&
-      userCoords.longitude <= cardinalPoints.east.longitude
+      userCoords.longitude <= cardinalPoints.east.longitude &&
+      matchesGender &&
+      matchesRank
     );
   });
 
@@ -102,16 +122,16 @@ export default function TabOneScreen() {
 
   const handleUserMarkerPress = (user: User) => {
     if (!currentLocation) return;
-  
+
     setSelectedUserId(user.id); // Marquez cet utilisateur comme sélectionné
     const userCoords = arrayToCoordinates(user.current_location);
     setSelectedUserCoords(userCoords);
-  
+
     const midPoint = {
       latitude: (currentLocation.latitude + userCoords.latitude) / 2,
       longitude: (currentLocation.longitude + userCoords.longitude) / 2,
     };
-  
+
     const nearestGym = filteredGyms.length
       ? findNearestLocation(midPoint, filteredGyms)
       : null;
@@ -123,28 +143,27 @@ export default function TabOneScreen() {
         userCoords,
         { latitude: nearestGym.equip_y, longitude: nearestGym.equip_x },
       ];
-    
+
       const latitudes = points.map((p) => p.latitude);
       const longitudes = points.map((p) => p.longitude);
-    
+
       const minLatitude = Math.min(...latitudes);
       const maxLatitude = Math.max(...latitudes);
       const minLongitude = Math.min(...longitudes);
       const maxLongitude = Math.max(...longitudes);
-    
+
       const paddingPercentage = 0.2;
       const latitudeDelta = (maxLatitude - minLatitude) * (1 + paddingPercentage);
       const longitudeDelta = (maxLongitude - minLongitude) * (1 + paddingPercentage);
-    
+
       const region = {
         latitude: (minLatitude + maxLatitude) / 2,
         longitude: (minLongitude + maxLongitude) / 2,
         latitudeDelta,
         longitudeDelta,
       };
-    
-      mapRef.current.animateToRegion(region, 800
-      );
+
+      mapRef.current.animateToRegion(region, 800);
       console.log(
         `Distance entre moi et le gymnase "${nearestGym.inst_nom}": ${calculateDistance(
           currentLocation,
@@ -152,26 +171,7 @@ export default function TabOneScreen() {
         ).toFixed(2)} km`
       );
     }
-    
   };
-
-  useEffect(() => {
-    if (usersInArea) {
-      Animated.sequence([
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: true,
-        }),
-        Animated.delay(4000),
-        Animated.timing(fadeAnim, {
-          toValue: 0,
-          duration: 500,
-          useNativeDriver: true,
-        }),
-      ]).start();
-    }
-  }, [usersInArea?.length]);
 
   const renderMap = () => {
     if (isLoading) {
@@ -219,7 +219,6 @@ export default function TabOneScreen() {
                   <Marker
                     key={`user-${index}`}
                     coordinate={arrayToCoordinates(user.current_location)}
-                    pinColor="green"
                     title={user.username}
                     description={`Classement: ${user.ranking}, Age: ${user.age}`}
                     onPress={() => handleUserMarkerPress(user)}
@@ -264,6 +263,13 @@ export default function TabOneScreen() {
                 />
               )}
             </MapView>
+            {/* Bouton pour ouvrir le modal */}
+            <TouchableOpacity
+              style={styles.filterButton}
+              onPress={() => setModalVisible(true)}
+            >
+              <Text style={styles.filterButtonText}>Filtrer</Text>
+            </TouchableOpacity>
           </View>
         ) : null;
       default:
@@ -275,9 +281,75 @@ export default function TabOneScreen() {
     }
   };
 
+  const toggleRank = (selectedRank: string) => {
+    setRankFilter((prev) =>
+      prev.includes(selectedRank) ? prev.filter((rank) => rank !== selectedRank) : [...prev, selectedRank]
+    );
+  };
+
   return (
     <View style={styles.container}>
       {renderMap()}
+      {/* Modal de filtrage */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+  <Text style={styles.modalTitle}>Filtrer les utilisateurs</Text>
+
+  {/* Filtre par sexe */}
+  <Text style={styles.filterLabel}>Sexe:</Text>
+  <ButtonGroup
+    buttons={['Tous', 'Homme', 'Femme']}
+    selectedIndex={
+      genderFilter === '' ? 0 : genderFilter === 'Homme' ? 1 : genderFilter === 'Femme' ? 2 : null
+    }
+    onPress={(index) => setGenderFilter(index === 0 ? '' : index === 1 ? 'Homme' : 'Femme')}
+    containerStyle={styles.buttonGroup}
+    selectedButtonStyle={styles.selectedButton}
+  />
+
+  {/* Filtre par classement */}
+  <Text style={styles.filterLabel}>Classement:</Text>
+  <ButtonGroup
+    buttons={ranks}
+    selectedIndexes={rankFilter.map((rank) => ranks.indexOf(rank))}
+    onPress={(index) => {
+      const selectedRank = ranks[index];
+      if (selectedRank === 'Tous') {
+        setRankFilter([]); // Réinitialise à "Tous"
+      } else {
+        toggleRank(selectedRank);
+      }
+    }}
+    containerStyle={styles.buttonGroup}
+    selectedButtonStyle={styles.selectedButton}
+  />
+
+  {/* Bouton Reset */}
+  <TouchableOpacity
+    style={styles.resetButton}
+    onPress={() => {
+      setGenderFilter('');
+      setRankFilter([]);
+    }}
+  >
+    <Text style={styles.resetButtonText}>Réinitialiser les filtres</Text>
+  </TouchableOpacity>
+
+  {/* Bouton Fermer */}
+  <TouchableOpacity
+    style={styles.closeButton}
+    onPress={() => setModalVisible(false)}
+  >
+    <Text style={styles.closeButtonText}>Fermer</Text>
+  </TouchableOpacity>
+</View>
+
+      </Modal>
     </View>
   );
 }
@@ -296,4 +368,71 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  filterButton: {
+    position: "absolute",
+    top: 50,
+    right: 20,
+    padding: 10,
+    backgroundColor: "blue",
+    borderRadius: 5,
+  },
+  filterButtonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    backgroundColor: "white",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginVertical: 5,
+  },
+  buttonGroup: {
+    marginVertical: 10,
+    borderRadius: 5,
+  },
+  selectedButton: {
+    backgroundColor: '#2196F3', // Couleur de fond du bouton sélectionné
+  },
+  filterLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  closeButton: {
+    width: "80%",
+    padding: 15,
+    backgroundColor: "red",
+    marginVertical: 5,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  resetButton: {
+    width: "80%",
+    padding: 15,
+    backgroundColor: "#FFA500", // Orange ou une couleur qui contraste bien
+    marginVertical: 10,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  resetButtonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  
 });
